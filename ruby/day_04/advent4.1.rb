7ae4135b660c064365564415c4c1dbe335535ef0
Sean Handley <sean.handley@gmail.com>
2019-12-04 07:30:16 +0000

    Refactor.

--- ruby/day_04/advent4.1.rb
+++ ruby/day_04/advent4.1.rb
@@ -1,15 +1,34 @@
 #!/usr/bin/env ruby
 
+# --- Day 4: Secure Container ---
+#
+# You arrive at the Venus fuel depot only to discover it's protected by a password. The Elves had written the password on a sticky note, but someone threw it out.
+#
+# However, they do remember a few key facts about the password:
+#
+# It is a six-digit number.
+# The value is within the range given in your puzzle input.
+# Two adjacent digits are the same (like 22 in 122345).
+# Going from left to right, the digits never decrease; they only ever increase or stay the same (like 111123 or 135679).
+#
+# Other than the range rule, the following are true:
+#
+# 111111 meets these criteria (double 11, never decreases).
+# 223450 does not meet these criteria (decreasing pair of digits 50).
+# 123789 does not meet these criteria (no double).
+#
+# How many different passwords within the range given in your puzzle input meet these criteria?
 class PasswordValidator
   attr_reader :range
 
-  def initialize(input, rules = nil)
-    @range = Range.new(*input.split("-"))
+  def initialize(start, stop, rules = nil)
+    @range = Range.new(start, stop)
     @rules = Rules.new(rules)
   end
 
   def add_rule(rule)
     @rules.add(rule)
+    self
   end
 
   def valid_password_count
@@ -32,6 +51,7 @@ class PasswordValidator
         true
       end
     ]
+
     def initialize(rules = nil)
       @rules = rules || DEFAULT_RULES
     end
@@ -46,6 +66,8 @@ class PasswordValidator
   end
 end
 
+INPUT = STDIN.read.split("-").freeze
+
 if __FILE__ == $0
-  p PasswordValidator.new(STDIN.read).valid_password_count
+  p PasswordValidator.new(*INPUT).valid_password_count
 end
--- ruby/day_04/advent4.2.rb
+++ ruby/day_04/advent4.2.rb
@@ -1,22 +1,28 @@
 #!/usr/bin/env ruby
 
+# --- Part Two ---
+#
+# An Elf just remembered one more important detail: the two adjacent matching digits are not part of a larger group of matching digits.
+#
+# Given this additional criterion, but still ignoring the range rule, the following are now true:
+#
+# 112233 meets these criteria because the digits never decrease and all repeated digits are exactly two digits long.
+# 123444 no longer meets the criteria (the repeated 44 is part of a larger group of 444).
+# 111122 meets the criteria (even though 1 is repeated more than twice, it still contains a double 22).
+#
+# How many different passwords within the range given in your puzzle input meet all of the criteria?
+
 require_relative "./advent4.1"
 
 runs = -> (password) do
-  runs = []
-  run = []
-  password.to_s.chars.each do |c|
-    if run.empty? || run.include?(c)
-      run << c
+  password.chars.each_with_object([[]]) do |char, runs|
+    run = runs.last
+    if run.empty? || run.include?(char)
+      run.append(char)
     else
-      runs << run
-      run = [c]
+      runs.append([char])
     end
-  end
-  runs << run
-  runs.map(&:count).any? { |count| count == 2 }
+  end.any? { |run| run.count == 2 }
 end
 
-pv = PasswordValidator.new(STDIN.read)
-pv.add_rule(runs)
-p pv.valid_password_count
+p PasswordValidator.new(*INPUT).add_rule(runs).valid_password_count
