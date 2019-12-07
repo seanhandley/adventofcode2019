6ee39bf4bc7b556cbac3bac63d98860c9c663bfa
Sean Handley <sean.handley@gmail.com>
2019-12-07 20:06:41 +0000

    Cleaning up.

--- ruby/day_01/part1.exs
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env elixir
-
-IO.read(:stdio, :all)
-|> String.split("\n")
-|> Enum.map(&String.to_integer/1)
-|> Enum.map(fn (weight) -> div(weight, 3) - 2 end)
-|> Enum.sum()
-|> IO.inspect()
