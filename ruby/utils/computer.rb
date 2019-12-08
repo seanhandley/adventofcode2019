47f0759435dec36da457cf28a70fe55490f18338
Sean Handley <sean.handley@gmail.com>
2019-12-08 21:28:01 +0000

    Refactor.

--- ruby/utils/computer.rb
+++ ruby/utils/computer.rb
@@ -3,7 +3,7 @@ require "securerandom"
 class Computer
   def initialize(program: nil, output: nil, id: nil, debug: false)
     @id = id || SecureRandom.hex(4)
-    @queue = Queue.new
+    @in = Queue.new
     @memory = program || Computer.fetch_program_from_stdin
     @output = output || -> (msg) { puts msg }
     @pos = 0
@@ -11,7 +11,7 @@ class Computer
   end
 
   def receive(input)
-    @queue << input
+    @in << input
     self
   end
 
@@ -45,16 +45,13 @@ class Computer
 
   def operands
     executable_instruction.arity.times.map do |i|
-      if operand_modes[i] == 1 || instr_number == 3 # hack!
-        loc = @pos + i + 1
-        @memory[loc].tap do |val|
-          debug "READ[#{loc}] => #{val}"
-        end
-      else
-        loc = @memory[@pos + i + 1]
-        @memory[loc].tap do |val|
-          debug "READ[#{loc}] => #{val}"
-        end
+      loc = if operand_modes[i] == 1 || instr_number == 3 # hack!
+              @pos + i + 1
+            else
+              @memory[@pos + i + 1]
+            end
+      @memory[loc].tap do |val|
+        debug "READ[#{loc}] => #{val}"
       end
     end
   end
@@ -75,8 +72,8 @@ class Computer
     {
       1 => -> (a, b, c) { store(a + b, c); @pos += 4 },
       2 => -> (a, b, c) { store(a * b, c); @pos += 4 },
-      3 => -> (a) { store(@queue.pop, a); @pos += 2 },
-      4 => -> (a) { write_output(a); @pos += 2 },
+      3 => -> (a) { store(@in.pop, a); @pos += 2 },
+      4 => -> (a) { @output.call(a); @pos += 2 },
       5 => -> (a, b) { a.nonzero? ? @pos = b : @pos += 3 },
       6 => -> (a, b) { a.zero? ? @pos = b : @pos += 3 },
       7 => -> (a, b, c) { store(a < b ? 1 : 0, c) ; @pos += 4 },
@@ -102,10 +99,6 @@ class Computer
       8 => "EQ",
       99 => "HALT",
     }[number]
-  end
-
-  def write_output(msg)
-    @output.call(msg)
   end
 
   def debug(msg)
