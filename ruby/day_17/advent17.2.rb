089cd9a0c81e03858f502c0312b00bece5847cf2
Sean Handley <sean.handley@gmail.com>
2019-12-27 10:54:44 +0000

    Refactor

--- ruby/day_17/advent17.2.rb
+++ ruby/day_17/advent17.2.rb
@@ -4,19 +4,10 @@ require_relative "../utils/computer"
 
 @program = Computer.fetch_program_from_stdin
 @program[0] = 2
-
-@dust = 0
-
-@output = -> (data) do
-  @dust = data
-end
-
 @instructions = File.read("instructions.txt").chars.map(&:ord)
 
-@computer = Computer.new(output: @output, program: @program, debug: false)
-@instructions.each do |instruction|
-  @computer.receive(instruction)
-end
+@computer = Computer.new(output: -> (o) { @dust = o }, program: @program)
+@instructions.each(&@computer.method(:receive))
 @computer.execute
 
 p @dust
